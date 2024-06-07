f5bd57391c44137ef99ca68190b0c1950156d60a
Armen <armdevjs@gmail.com>
2024-06-07 12:15:48 +0400

    feat: image to next image

--- app/auth/sign-in/layout.tsx
+++ app/auth/sign-in/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -8,12 +9,23 @@ const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main
       className={twMerge(
-        userName
-          ? "bg-[url('/images/login_background_second.jpg')]"
-          : "bg-[url('/images/login_background.jpg')]",
+        // userName
+        //   ? "bg-[url('/images/login_background_second.jpg')]"
+        //   : "bg-[url('/images/login_background.jpg')]",
         "w-screen h-screen bg-no-repeat bg-cover bg-center relative px-2 sm:px-9 flex justify-center lg:justify-end items-center"
       )}
     >
+      <Image
+        src={
+          userName
+            ? "/images/login_background_second.jpg"
+            : "/images/login_background.jpg"
+        }
+        alt="Login background"
+        width={1090}
+        height={1080}
+        className="w-full h-full object-cover absolute left-0 top-0 -z-10"
+      />
       {children}
     </main>
   );
