74a1b46a6407f58c92163d25574e554ddaa01554
Armen <armdevjs@gmail.com>
2024-06-07 12:28:51 +0400

    feat: sign up page image

--- app/auth/sign-up/layout.tsx
+++ app/auth/sign-up/layout.tsx
@@ -2,10 +2,17 @@
 
 import { ReactNode } from "react";
 import ZeedeoSticker from "../Components/ZeedeoSticker";
+import Image from "next/image";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="w-screen h-screen bg-[url('/images/sign_up_background.jpg')] bg-no-repeat bg-cover bg-center relative px-2 sm:px-9 flex flex-col lg:justify-center items-center lg:items-end overflow-y-auto">
+    <main className="w-screen h-screen bg-no-repeat bg-cover bg-center relative px-2 sm:px-9 flex flex-col lg:justify-center items-center lg:items-end overflow-y-auto">
+      <Image
+        src="/images/sign_up_background.jpg"
+        alt="Login background"
+        priority
+        className="w-full h-full object-cover absolute left-0 top-0 -z-10 animate-smooth-appear"
+      />
       <ZeedeoSticker className="lg:absolute mx-auto sm:mx-[unset] left-9 bottom-9 mb-4" />
       {children}
     </main>
