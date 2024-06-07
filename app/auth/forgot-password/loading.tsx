0d013cafe84cedc4bec338967db30ffc99aef90f
Armen <armdevjs@gmail.com>
2024-06-07 12:03:47 +0400

    feat: add loading state

---
+++ app/auth/forgot-password/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Loading;
